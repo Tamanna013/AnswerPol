05a9dc163ce0bba3381c28746f48d15675e778f6
TashxMG <149794391+Tamanna013@users.noreply.github.com>
2024-12-15 18:43:28 +0530

    Update homepage.js

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,24 +1,25 @@
-import React from "react";
-import "./homepage.css";
+import React, { useState } from "react";
+import "./homepage.css"; // Import the CSS file for styling
 import logo from "./goapolice.png";
 
 const PoliceHelpDesk = () => {
+  const [isBotOpen, setIsBotOpen] = useState(false);
+
+  const openChatBot = () => {
+    setIsBotOpen(!isBotOpen); // Toggle chatbot visibility
+  };
+
   return (
     <div className="page-container">
-      {}
       <div className="logo-container">
-      <img src={logo} alt="Goa Police Logo" className="logo" />;
+        <img src={logo} alt="Goa Police Logo" className="logo" />
       </div>
 
-      {}
       <h1 className="title">
         Answer<span className="highlight">Pol</span>
       </h1>
-      <p className="subtitle">
-        At your service ^^
-      </p>
+      <p className="subtitle">At your service ^^</p>
 
-      {}
       <div className="input-container">
         <input
           type="text"
@@ -31,6 +32,35 @@ const PoliceHelpDesk = () => {
           className="input-field"
         />
       </div>
+
+      <div className="chatbot-icon" onClick={openChatBot}>
+        {/* Inline SVG icon */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          className="chat-icon-svg"
+        >
+          <path
+            d="M19 19l-2-2H5a2 2 0 01-2-2V5a2 2 0 012-2h14a2 2 0 012 2v12a2 2 0 01-2 2z"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </div>
+
+      {isBotOpen && (
+        <div className="chatbot-popup">
+          <iframe
+            src="https://cdn.botpress.cloud/webchat/v2.2/shareable.html?configUrl=https://files.bpcontent.cloud/2024/12/15/12/20241215125517-QJ3NU21K.json"
+            width="100%"
+            height="100%"
+            title="Chatbot"
+          ></iframe>
+        </div>
+      )}
     </div>
   );
 };
