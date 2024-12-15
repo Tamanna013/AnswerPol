f6ad754de1b7065972099b44c38ff1bef29cf938
TashxMG <149794391+Tamanna013@users.noreply.github.com>
2024-12-15 19:06:49 +0530

    Update homepage.js

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -35,20 +35,7 @@ const PoliceHelpDesk = () => {
 
       <div className="chatbot-icon" onClick={openChatBot}>
         {}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          className="chat-icon-svg"
-        >
-          <path
-            d="M19 19l-2-2H5a2 2 0 01-2-2V5a2 2 0 012-2h14a2 2 0 012 2v12a2 2 0 01-2 2z"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
+        <i className="bi bi-robot"></i>
       </div>
 
       {isBotOpen && (
